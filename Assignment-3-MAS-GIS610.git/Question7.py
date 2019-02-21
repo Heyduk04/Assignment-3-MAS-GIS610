a26ee86f1da99d99e5264d1d8f524902e7a141d4
Heyduk04 <47437819+Heyduk04@users.noreply.github.com>
2019-02-20 17:40:29 -0700

    Update Question7.py

--- Assignment-3-MAS-GIS610.git/Question7.py
+++ Assignment-3-MAS-GIS610.git/Question7.py
@@ -1,12 +1,16 @@
-# This question took me the longest to complete and I still couldn't get it to run correclty. The examples on 
-# http://pro.arcgis.com/en/pro-app/tool-reference/data-management/select-layer-by-attribute.htm were not helpful either.
-
 import arcpy 
+
 arcpy.env.workspace = r'C:\Users\theyduk\Desktop\Heyduk_LabWork\610 Assignments\610 Exercise 3\Exercise 3.gdb'
-arcpy.env.overwriteOutput = True
-SelectFc = (r'C:\Users\theyduk\Desktop\Heyduk_LabWork\610 Assignments\610 Exercise 3\Exercise 3.gdb\General_Offense')
-fieldFc = 'OffenseCustom'
-AddFc = r'C:\Users\theyduk\Desktop\Heyduk_LabWork\610 Assignments\610 Exercise 3\Exercise 3.gdb\CallsforService'
-arcpy.MakeFeatureLayer_management('SelectFc','General_OffenseLayer_lyr')
-arcpy.SelectLayerByAttribute_management('General_OffenseLayer_lyr','NewLayer', 'x_rand > 10')
-arcpy.CopyFeatures_management('General_OffenseLayer_lyr',r'C:\Users\theyduk\Desktop\Heyduk_LabWork\610 Assignments\610 Exercise 3\Exercise 3.gdb\NewLayer')
+arcpy.env.overwriteOutput = True 
+
+inFeatures = r'C:\Users\theyduk\Desktop\Heyduk_LabWork\610 Assignments\610 Exercise 3\Exercise 3.gdb\CallsforService'
+
+outLocation = 'FeaturetoFeature'
+
+outFeatureClass = r'C:\Users\theyduk\Desktop\Heyduk_LabWork\610 Assignments\610 Exercise 3\Exercise 3.gdb\General_Offense'
+
+arcpy.MakeFeatureLayer_management(inFeatures, 'CallsforService_lyr')
+
+arcpy.SelectLayerByAttribute_management('CallsforService_lyr','NEW_SELECTION','x_rand >10')
+
+arcpy.CopyFeatures_management('CallsforService_lyr',r'C:\Users\theyduk\Desktop\Heyduk_LabWork\610 Assignments\610 Exercise 3\Exercise 3.gdb\New_Selection')
