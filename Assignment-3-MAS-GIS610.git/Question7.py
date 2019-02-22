e3716a55e647c4abd2dbb83119b6f3d1f0863bda
Heyduk04 <47437819+Heyduk04@users.noreply.github.com>
2019-02-21 19:23:28 -0700

    Update Question7.py

--- Assignment-3-MAS-GIS610.git/Question7.py
+++ Assignment-3-MAS-GIS610.git/Question7.py
@@ -7,10 +7,8 @@ inFeatures = r'C:\Users\theyduk\Desktop\Heyduk_LabWork\610 Assignments\610 Exerc
 
 outLocation = 'FeaturetoFeature'
 
-outFeatureClass = r'C:\Users\theyduk\Desktop\Heyduk_LabWork\610 Assignments\610 Exercise 3\Exercise 3.gdb\General_Offense'
-
 arcpy.MakeFeatureLayer_management(inFeatures, 'CallsforService_lyr')
 
-arcpy.SelectLayerByAttribute_management('CallsforService_lyr','NEW_SELECTION','x_rand >10')
+arcpy.SelectLayerByAttribute_management('CallsforService_lyr','NEW_SELECTION',"CFSType = 'Assault Call'")
 
 arcpy.CopyFeatures_management('CallsforService_lyr',r'C:\Users\theyduk\Desktop\Heyduk_LabWork\610 Assignments\610 Exercise 3\Exercise 3.gdb\New_Selection')
